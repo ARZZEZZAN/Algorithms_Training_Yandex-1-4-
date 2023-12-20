c72df5546aa2ba7fdc3e57963f76b9008de41873
Armen Arzumanyan <armen.arzumanyan@tibbo.com>
2023-12-20 21:44:42 +0300

    F(Done)

--- Algorithms_Training-01/Homework02/SymmetricSequence.java
+++ Algorithms_Training-01/Homework02/SymmetricSequence.java
@@ -21,23 +21,38 @@ public class SymmetricSequence {
     }
 
     public static void symmetricSequence(ArrayList<Integer> nums) {
-        int count = 1;
+        int index = nums.size();
+        boolean needToCheck = false;
+        ArrayList<Integer> result = new ArrayList<>();
 
         for (int i = 0; i < nums.size(); i++) {
-            if (!Objects.equals(nums.get(i), nums.get(nums.size() - (i + 1)))) {
-                while (Objects.equals(nums.get(nums.size() - (i + 1)), nums.get(nums.size() - (i + (count+1))))) {
-                    count++;
-                }
-                ArrayList<Integer> result = new ArrayList<>(nums.subList(0, nums.size() - count));
-                Collections.reverse(result);
+            if (!Objects.equals(nums.get(i), nums.get(nums.size() - (i + 1))) || needToCheck) {
+                result.add(nums.get(i));
+                nums.add(index, nums.get(i));
+                if(isSymmetric(nums)) {
+                    Collections.reverse(result);
 
-                System.out.println(result.size());
-                System.out.print(result.stream().map(String::valueOf).collect(Collectors.joining(" ")));
-                return;
+                    System.out.println(result.size());
+                    System.out.print(result.stream().map(String::valueOf).collect(Collectors.joining(" ")));
+                    return;
+                }
+                needToCheck = true;
             }
         }
 
         System.out.println(0);
+    }
+
+    private static boolean isSymmetric(ArrayList<Integer> nums) {
+        int count = 0;
+        while (Objects.equals(nums.get(count), nums.get(nums.size() - (count + 1)))) {
+            count++;
+
+            if(count == nums.size()) {
+                return true;
+            }
+        }
+        return false;
     }
 }
 //\\ 1 2 3 4 5 6 7 8 7 6 5 4 3 2 1 2 3 4 5 6 7 8
