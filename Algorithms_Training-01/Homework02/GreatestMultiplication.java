ec0622691d5047eda402b5d0888b411f9a8d896b
Armen Arzumanyan <armen.arzumanyan@tibbo.com>
2023-12-26 21:20:49 +0300

    G - задание(начало)

---
+++ Algorithms_Training-01/Homework02/GreatestMultiplication.java
@@ -0,0 +1,30 @@
+package Homework02;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.concurrent.atomic.AtomicReferenceArray;
+import java.util.stream.Collectors;
+
+import com.sun.source.tree.Tree;
+
+public class GreatestMultiplication {
+    public static void main(String[] args) throws Exception {
+        Scanner sScanner = new Scanner(System.in);
+
+        String input = sScanner.nextLine();
+        String[] numsLine = input.split(" ");
+
+        greatestMultiplication(numsLine, 2);
+    }
+
+    public static void greatestMultiplication(String[] numsLine, int amount) {
+       for (String element : numsLine) {
+
+       }
+    }
+
+}
